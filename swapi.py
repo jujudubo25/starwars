f0da5876bbf523fa14f57bc64bc8f91f15ab91b8
jujudubo25 <jujudubo25@gmail.com>
2018-05-04 13:19:01 -0700

    implement get_vehicle function.

--- swapi.py
+++ swapi.py
@@ -1,3 +1,15 @@
 import json
 import requests
 
+def get_vehicle(index):
+	"""
+	A function that gives the name of a vehicle specific to a certain person.
+	"""
+	data = requests.get('https://www.swapi.co/api/people/%d' % (index))
+	data = json.loads(data.text)
+	try:
+		for x in range(len(data['vehicles'])):
+			data['vehicles'][x] = info(data['vehicles'][x])
+		return data['vehicles']
+	except  IndexError:
+		return None
